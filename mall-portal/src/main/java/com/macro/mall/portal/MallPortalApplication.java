3d0b6227ce7b04b2a54f79b6c424e1ea74165422
chenjun <825766568@qq.com>
2020-08-28 10:57:53 +0800

    update 修改配置文件的数据库连接方式

--- mall-portal/src/main/java/com/macro/mall/portal/MallPortalApplication.java
+++ mall-portal/src/main/java/com/macro/mall/portal/MallPortalApplication.java
@@ -1,13 +1,34 @@
 package com.macro.mall.portal;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+@Slf4j
 @SpringBootApplication(scanBasePackages = "com.macro.mall")
-public class MallPortalApplication {
+public class MallPortalApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 
-    public static void main(String[] args) {
-        SpringApplication.run(MallPortalApplication.class, args);
+        return application.sources(MallPortalApplication.class);
+    }
+
+    public static void main(String[] args)  throws UnknownHostException {
+        ConfigurableApplicationContext application = SpringApplication.run(MallPortalApplication.class, args);
+        Environment env = application.getEnvironment();
+        String ip = InetAddress.getLocalHost().getHostAddress();
+        String port = env.getProperty("server.port");
+        log.info("\n----------------------------------------------------------\n\t" +
+                "Application Mall-portal is running! Access URLs:\n\t" +
+                "Local: \t\thttp://localhost:" + port +  "/\n\t" +
+                "swagger-ui: \thttp://" + ip + ":" + port + "/swagger-ui.html\n\t" +
+                "----------------------------------------------------------");
     }
 
 }
